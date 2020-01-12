cd238a7a5290561ff8cc3ef0ee929a4a3422a977
jake richardson <jake.ejrichardson@gmail.com>
2020-01-12 00:38:06 -0500

    finished Base parser
    alakzsm

--- parser.js
+++ parser.js
@@ -13,24 +13,55 @@ function processData() {
         rows = results.data;
         }
     });
-
+    
     //loop through the excel array to remove oddities
     for(var row = 0; row < rows.length; row++){
-        console.log(rows[row])
         for(var column = 0; column < rows[row].length; column++){
             str = rows[row][column]
 
             //remove any classess that are full
-            if(str.includes("Full")){
+            if( str.substring(2, 6) == "Full"){
                 rows.splice(row, 3)
             }
             
-            //remove any classes that have a tab infront of data
-            else if(str.includes("\t")){
+            //remove any  tabs infront of data
+            else if(str.substring(1,2 ) == ("\t")){
                 rows[row][column] = str.toString().replace('\t', '')
             }
         }
-    }
+    }   
+    //parse Data
+    Courses = parseData(rows)
+    
+    console.log(Courses)
 }
 
+function parseData(array){
+     //array to store the parsed courses 
+     let Courses = []
+     for(let row = 0; row < array.length; row++){
+         //current course
+         let Course = []
+         //check if it is correct section to parse
+         if (array[row][1] == "Open"){
+             //get current row
+             let info = array[row] 
+             Course.push(info[3], info[7], info[10])
+             
+             //find days
+             let info2 = array[row+1][0]
+             let start1 = info2.indexOf("Days:") + 5
+             let fin1 = info2.indexOf("Time") +5
+             Course.push(info2.substring(start1, fin1))
+ 
+             //find times
+             let fin2 =  info2.indexOf("Building")
+             Course.push(info2.substring(fin1, fin2))
+             Courses.push(Course)
+ 
+         }
+ 
+     }
+     return Courses
+}
 processData();
