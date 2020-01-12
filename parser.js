10f85882fef284eb95392be205e2913f2f72fae2
jake richardson <jake.ejrichardson@gmail.com>
2020-01-12 01:16:49 -0500

    converted to a class
    shoulda started like that

--- parser.js
+++ parser.js
@@ -1,67 +1,80 @@
-function processData() {
+class DataProcess {
 
-    var Papa = require('papaparse');
-    const fs = require('fs');
+    constructor(){
+        this.array = []
+        this.ParsedArray = []
+        this.Courses = []
+        this.JSONdata = null
+    }
 
-    var file = './webscraper/courses.csv';
-    var content = fs.readFileSync(file, "utf8");   
-
-    Papa.parse(content, {
-        header: false,
-        delimiter: "",
-        complete: function(results) {
-        rows = results.data;
-        }
-    });
+    processData() {
+        var Papa = require('papaparse');
+        const fs = require('fs');
     
-    //loop through the excel array to remove oddities
-    for(var row = 0; row < rows.length; row++){
-        for(var column = 0; column < rows[row].length; column++){
-            str = rows[row][column]
+        var file = './webscraper/courses.csv';
+        var content = fs.readFileSync(file, "utf8");   
+        
+        let rows = []
+        Papa.parse(content, {
+            header: false,
+            delimiter: "",
+            complete: function(results) {
+                rows = results.data;
+            }
+        })
 
-            //remove any classess that are full
-            if( str.substring(2, 6) == "Full"){
-                rows.splice(row, 3)
-            }
-            
-            //remove any  tabs infront of data
-            else if(str.substring(1,2 ) == ("\t")){
-                rows[row][column] = str.toString().replace('\t', '')
-            }
-        }
-    }   
-    //parse Data
-    Courses = parseData(rows)
+        this.array = rows
+        //loop through the excel array to remove oddities
+        for(var row = 0; row < this.array.length; row++){
+            for(var column = 0; column < this.array[row].length; column++){
+                let str = this.array[row][column]
     
-    console.log(Courses)
-}
-
-function parseData(array){
-     //array to store the parsed courses 
-     let Courses = []
-     for(let row = 0; row < array.length; row++){
-         //current course
-         let Course = []
-         //check if it is correct section to parse
-         if (array[row][1] == "Open"){
-             //get current row
-             let info = array[row] 
-             Course.push(info[3], info[7], info[10])
-             
-             //find days
-             let info2 = array[row+1][0]
-             let start1 = info2.indexOf("Days:") + 5
-             let fin1 = info2.indexOf("Time") +5
-             Course.push(info2.substring(start1, fin1))
- 
-             //find times
-             let fin2 =  info2.indexOf("Building")
-             Course.push(info2.substring(fin1, fin2))
-             Courses.push(Course)
- 
+                //remove any classess that are full
+                if( str.substring(2, 6) == "Full"){
+                    this.array.splice(row, 3)
+                }
+                
+                //remove any  tabs infront of data
+                else if(str.substring(1,2 ) == ("\t")){
+                    this.array[row][column] = str.toString().replace('\t', '')
+                }
+            }
+        }  
+        console.log(this.array) 
+        //parse Data
+        this.parseData()
+        //covert to json
+        this.JSONdata = JSON.stringify(this.Courses)
+       
+    
+    }
+    
+    //funciton to parse the data from the array into seperate courses in an array
+    parseData(){
+         //array to store the parsed courses 
+         for(let row = 0; row < this.array.length; row++){
+             //current course
+             let Course = []
+             //check if it is correct section to parse
+             if (this.array[row][1] == "Open"){
+                 //get current row
+                 let info = this.array[row] 
+                 Course.push(info[3], info[7], info[10])
+                 
+                 //find days
+                 let info2 = this.array[row+1][0]
+                 let start1 = info2.indexOf("Days:") + 5
+                 let fin1 = info2.indexOf("Time") +5
+                 Course.push(info2.substring(start1, fin1))
+     
+                 //find times
+                 let fin2 =  info2.indexOf("Building")
+                 Course.push(info2.substring(fin1, fin2))
+                 this.Courses.push(Course)
+             }
          }
- 
-     }
-     return Courses
+    } 
 }
-processData();
+
+tester = new DataProcess
+tester.processData()
