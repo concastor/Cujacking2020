54d468bfd043ac1e3073680d0c48756b8c8b57e0
Ben <ben.vandenberg@carleton.ca>
2020-05-12 23:20:51 -0400

    Moved backend to  its own folder, removed react basis, moving to Vue

---
+++ backend/routes/users.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+// url path /users/
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.status(200).json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// url path /users/add
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const isAdmin = Boolean(req.body.isAdmin);
+
+    const newUser = new User({username, isAdmin});
+
+    newUser.save()
+        .then(() => res.status(201).json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// get user specific details
+router.route('/:username').get((req, res) => {
+    User.findById(req.params.username)
+        // User.find({username: req.params.username})
+        .then(user => res.status(200).json(user))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+// Update as post
+router.route('/:username').post((req, res) => {
+    User.findById(req.params.username)
+        // User.find({username: req.params.username})
+        .then(user => {
+            user.username = req.body.username;
+            user.isAdmin = req.body.isAdmin;
+
+            user.save()
+                .then(() => res.status(202).json("User Updated!"))
+                .catch(err => res.status(400).json("Error: " + err));
+        });
+});
+
+// delete user
+router.route('/:username').delete((req, res) => {
+    User.findById(req.params.username)
+        // User.find({username: req.params.username})
+        .then(user => {
+            user.delete()
+                .then(() => res.status(202).json("User Deleted!"))
+                .catch(err => res.status(400).json("Error: " + err));
+        });
+});
+
+module.exports = router;
--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-Run "npm install" and "npm install https://npm.daypilot.org/daypilot-pro-react/trial/2019.3.4012.tar.gz --save"
-
-start server with "npm start"
