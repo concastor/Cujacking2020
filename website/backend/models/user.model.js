5c61373e899cffd0baec5a431f68ad124ec48cd6
Ben <ben.vandenberg@carleton.ca>
2020-05-13 02:00:53 -0400

    clean up code

--- website/backend/models/user.model.js
+++ website/backend/models/user.model.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-        minlength: 3
-    }, isAdmin: { type: Boolean, required: true }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  isAdmin: { type: Boolean, required: true }
 }, {
-    timestamps: true,
-});
+  timestamps: true
+})
 
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
